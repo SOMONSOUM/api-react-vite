54eb2e7f20a660bba3b0b8bea294f1f37dd78d85
SOMON SOUM <somon.soum9@gmail.com>
2023-06-06 11:38:44 +0700

    first commit

---
+++ src/server.ts
@@ -0,0 +1,41 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { Database, Entity } from "fakebase";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const db = new Database("./data");
+interface User extends Entity {
+  name: string;
+  status: boolean;
+}
+
+export const User = db.table<User>("users");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+
+app.get("/", (req, res) => {
+  res.send(
+    `<h1 style='text-align: center; color:blue; font-weight: bold'> Welcome to API!</h1>`
+  );
+});
+
+app.post("/create", async (req: Request, res: Response) => {
+  const { name, status } = req.body;
+
+  const user = await User.create({
+    name: name,
+    status: status,
+  });
+  return user;
+});
+
+app
+  .listen(PORT, () => {
+    console.log(`🚀Server started on http://localhost:${PORT}`);
+  })
+  .on("error", (error: any) => console.error(error));
